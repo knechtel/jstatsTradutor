3203772e921a1327d5553066b80675d5d36652b4
Maiquel Knechtel <maiquelknechtel@MacBook-Pro-de-Maiquel.local>
2018-09-05 17:34:53 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+
+public class Main extends Object{
+	
+
+	String texto = "literal";
+	String textoDois = "literal";
+
+	public static void main(String args[]){
+	
+		/*
+		
+		
+		*/
+	
+	}
+}
+
+
+
+class Test{
+	String test(){
+		return "maiquel";
+	}
+	
+}
